b50f2190c7a51598be53caad304bca42759e0f23
Kirin Inoue <kirin1013qilin@gmail.com>
2025-09-07 15:57:05 +0900

    commit

--- Hiroo App/SideMenuViewController.swift
+++ Hiroo App/SideMenuViewController.swift
@@ -1,7 +1,22 @@
+//
+//  MenuViewController.swift
+//  Hiroo App
+//
+//  Created by 井上　希稟 on 2025/09/03.
+//
+
+// MenuViewController.swift
+//
+//  MenuViewController.swift
+//  Hiroo App
+//
+//  Created by XX on 2025/09/03.
+//
+
 import UIKit
 
 final class SideMenuViewController: UITableViewController {
-    private let items = ["学校選択に戻る", "イベント一覧", "設定"]
+    private let items = [NSLocalizedString("back", comment: ""), "校内マップ", "ホームページ"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,20 +42,26 @@ final class SideMenuViewController: UITableViewController {
         let selectedItem = items[indexPath.row]
         
         switch selectedItem {
-        case "Back to Home Screen":
+        case "学校選択に戻る":
             self.presentingViewController?.dismiss(animated: true, completion: { [weak self] in
                 self?.routeToSelectSchool()
             })
-        case "School Flooring":
-            self.presentingViewController?.dismiss(animated: true, completion: {
-                if let nav = UIApplication.shared.windows.first?.rootViewController as? UINavigationController {
-                    let schoolVC = SchoolsViewController()
-                    nav.pushViewController(schoolVC, animated: true)
-                }
+        case
+            "校内マップ":
+            self.presentingViewController?.dismiss(animated: true, completion: { [weak self] in
+                self?.routeToSchoolMap()
             })
-
-        case "設定":
-            print("設定 tapped")
+        case
+            "ホームページ":
+            self.presentingViewController?.dismiss(animated: true, completion: { [weak self] in
+                self?.routeToSelectSchool()
+            })
+            
+//        case "イベント一覧":
+//            print("イベント一覧 tapped")
+//
+//        case "設定":
+//            print("設定 tapped")
             
         default:
             break
@@ -49,6 +70,24 @@ final class SideMenuViewController: UITableViewController {
     private func routeToSelectSchool() {
         let sb = UIStoryboard(name: "Main", bundle: nil)
         guard let selectVC = sb.instantiateViewController(withIdentifier: "SelectSchoolViewController") as? SelectSchoolViewController else {
+            return
+        }
+        let nav = UINavigationController(rootViewController: selectVC)
+        if let windowScene = view.window?.windowScene,
+           let sceneDelegate = windowScene.delegate as? SceneDelegate,
+           let window = sceneDelegate.window {
+            window.rootViewController = nav
+            window.makeKeyAndVisible()
+        } else {
+            if let window = UIApplication.shared.windows.first(where: { $0.isKeyWindow }) {
+                window.rootViewController = nav
+                window.makeKeyAndVisible()
+            }
+        }
+    }
+    private func routeToSchoolMap() {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        guard let selectVC = sb.instantiateViewController(withIdentifier: "SchoolsViewController") as? SchoolsViewController else {
             return
         }
         let nav = UINavigationController(rootViewController: selectVC)
