a71ed8d403ddb625e934eb210303e5b633a4de31
Ui <138472001+Ui-Sasaki@users.noreply.github.com>
2025-09-07 14:44:44 +0900

    Add files via upload

---
+++ Hiroo App/SchoolsViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class SchoolsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+        title = "School Flooring"
+    }
+}
